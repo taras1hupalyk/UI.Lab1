22a4698c40ec11cada6b71234ad182e664f37f9d
taras1hupalyk <t.hupalyk@gmail.com>
2023-03-12 19:25:37 +0200

    Add project files.

---
+++ DDC.Client/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using DDC.Client.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDC.Client.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DebtViewCommand { get; set; }
+        public RelayCommand DepositViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public DebtViewModel DebtVM { get; set; }
+        public DepositViewModel DepositVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new();
+            DebtVM = new();
+            DepositVM = new();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DebtViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DebtVM;
+            });
+
+            DepositViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DepositVM;
+            });
+
+
+        }
+
+        public override string? ToString()
+        {
+            return "hello im a viewmodel, probably the main one and i will appear in the main window";
+        }
+    }
+}
