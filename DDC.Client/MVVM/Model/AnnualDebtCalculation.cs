75a5e25ae22d05a4240aaca014110f0d68ca171e
taras1hupalyk <t.hupalyk@gmail.com>
2023-03-15 16:37:35 +0200

    Implement Strategy patter to calculation process. This provide ability to choose calculation method and make more easy adding new calculationn methods in the future.

---
+++ DDC.Client/MVVM/Model/AnnualDebtCalculation.cs
@@ -0,0 +1,27 @@
+﻿using DDC.Client.MVVM.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDC.Client.MVVM.Model
+{
+    class AnnualDebtCalculation : IBankingCalculation
+    {
+        public CalculationResult Calculate(decimal debtAmount, decimal interestRate, int months)
+        {
+            decimal monthlyInterestRate = interestRate / 100 / 12;
+            decimal monthlyPayment = debtAmount * (monthlyInterestRate + (monthlyInterestRate / ((decimal)Math.Pow((double)(1 + monthlyInterestRate), months) - 1)));
+            decimal totalInterest = monthlyPayment * months - debtAmount;
+            decimal totalSum = debtAmount + totalInterest;
+
+            return new CalculationResult
+            {
+                MonthlyPayment = monthlyPayment,
+                TotalInterest = totalInterest,
+                TotalSum = totalSum
+            };
+        }
+    }
+}
---
+++ DDC.Client/MVVM/Model/CalculationService.cs
@@ -0,0 +1,28 @@
+﻿using DDC.Client.MVVM.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDC.Client.MVVM.Model
+{
+    class CalculationService
+    {
+        private IBankingCalculation _calculationMethod;
+
+        public void SetCalculationMethod(IBankingCalculation calculationMethod)
+        {
+            _calculationMethod = calculationMethod;
+        }
+
+
+        public CalculationResult Calculate(decimal debtAmount, decimal interestRate, int months)
+        {
+            return _calculationMethod.Calculate(debtAmount, interestRate, months);
+        }
+
+
+        
+    }
+}
